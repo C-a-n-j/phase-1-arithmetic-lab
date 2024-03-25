2d864efc45a5283258cc4cdbcfb6f87d7ad5a212
Eddiezon-lemayan <Eddymacharia450@gmail.com>
2024-03-25 19:22:30 +0300

    complete task

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
+let num1 = 31;
+let num2 = 2;
+let multiply = num1 * num2;
+console.log(multiply);
 
-// Write your code here
+
+let random;
+random =Math.floor((Math.random() * 10)+1);
+console.log(random);
+
+
+let mod;
+let num3 = 14;
+let num4 = 5;
+mod =  num3 % num4;
+console.log(mod);
+
+
+let max;
+max = Math.max(8, 12, 16, 20);
+console.log(max);
+
